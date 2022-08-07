fbaaf4825db4c8104d1ae01292ada50cc5a6857d
mahesh9164 <110708587+mahesh9164@users.noreply.github.com>
2022-08-07 15:24:12 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("😇 About Me 😇", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close 🔒", callback_data = "close")
                 ]
             ]
         )
@@ -114,7 +114,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "🔰 Join Channel 🔰",
                 url = client.invitelink)
         ]
     ]
@@ -122,7 +122,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = '🔄 Try Again 🔄',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
